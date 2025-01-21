4fdedc27dad3e8ebc66e1a24bf40509b2dde1ef7
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2025-01-21 05:06:51 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,63 +1,76 @@
 import React from 'react';
-import { FaInstagram } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-// import { RiPrivacyPolicyLine } from 'react-icons/ri';
-
 
 export const Footer: React.FC = () => {
   return (
-    <footer id="contact" className="w-[1921px] h-[380px] bg-[#080808]  max-w-8xl mx-auto flex ">
+    <footer id="contact" className="bg-[#080808] max-w-8xl mx-auto flex justify-center items-center">
+      <div className="w-[1290px] flex flex-col gap-[60px]">
 
-<div className="w-[1290px] h-[380px] flex-col justify-start items-start gap-[60px] inline-flex max-w-8xl mx-auto ">
-  <div className="self-stretch pt-[60px] justify-start items-start gap-[94px] inline-flex">
-    <div className="w-[81px] flex-col justify-start items-start gap-2.5 inline-flex">
-      <div className="self-stretch text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">PUBLUU</div>
-      <div className="self-stretch text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">Home<br/>Reviews<br/>Price plan<br/>Try for free<br/>Site Map</div>
-    </div>
-    <div className="w-[120px] flex-col justify-start items-start gap-2.5 inline-flex">
-      <div className="self-stretch text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">Features</div>
-      <div className="self-stretch text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">Virtual Bookshelf<br/>Flipbook App<br/>Statistics<br/>PDF tracking</div>
-    </div>
-    <div className="w-[137px] flex-col justify-start items-start gap-2.5 inline-flex">
-      <div className="self-stretch text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">Legal</div>
-      <div className="self-stretch text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">Terms & conditions<br/>Privacy statement<br/>Cookie policy<br/>Refund policy<br/>Content Protection</div>
-    </div>
-    <div className="w-[138px] flex-col justify-start items-start gap-2.5 inline-flex">
-      <div className="self-stretch text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">Help</div>
-      <div className="self-stretch text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">What is a Flipbook?<br/>Help Guide<br/>Blog<br/>Contact</div>
-    </div>
-    <div className="w-[142px] flex-col justify-start items-start gap-2.5 inline-flex">
-      <div className="self-stretch text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">Solutions</div>
-      <div className="self-stretch text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">Flipbook Maker<br/>Online Catalog<br/>Magazine Maker<br/>Brochure Maker<br/>PDF Page Flip Effect</div>
-    </div>
-    <div className="w-[201px] flex-col justify-start items-start gap-2.5 inline-flex">
-      <div className="self-stretch text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]"> </div>
-      <div className="self-stretch text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">Business Proposal<br/>Ebook Maker<br/>Multimedia Presentation<br/>Company Newsletter Maker</div>
-    </div>
-  </div>
-  <div className="self-stretch h-[70px] pb-5 flex-col justify-start items-start gap-5 flex">
-    <div className="self-stretch justify-start items-center gap-[203px] inline-flex">
-      <div className="opacity-40 justify-start items-center gap-2.5 flex">
-        <div className="w-6 h-6 relative  overflow-hidden" />
-        {/* <div className="justify-start items-center gap-[5px] flex">
-          <div className="text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">English</div>
-          <div className="w-6 h-6 relative origin-top-left rotate-90  overflow-hidden" />
-        </div> */}
-      </div>
-      <div className="w-[501px] flex-col justify-start items-center inline-flex">
-        <div className="self-stretch opacity-40 text-center text-white/50 text-sm font-bold font-['Urbanist'] uppercase leading-[30px]">Convert your PDF into HTML5 Online flipbook with PAGE FLIP EFFECT.
+        <div className="flex justify-between pt-[60px]">
+        
+          <div className="flex flex-col gap-[10px]">
+            <h3 className="text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">PUBLUU</h3>
+            <p className="text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">
+              Home<br />Reviews<br />Price plan<br />Try for free<br />Site Map
+            </p>
+          </div>
+          
+          <div className="flex flex-col gap-[10px]">
+            <h3 className="text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">Features</h3>
+            <p className="text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">
+              Virtual Bookshelf<br />Flipbook App<br />Statistics<br />PDF tracking
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-[10px]">
+            <h3 className="text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">Legal</h3>
+            <p className="text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">
+              Terms & conditions<br />Privacy statement<br />Cookie policy<br />Refund policy<br />Content Protection
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-[10px]">
+            <h3 className="text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">Help</h3>
+            <p className="text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">
+              What is a Flipbook?<br />Help Guide<br />Blog<br />Contact
+            </p>
+          </div>
+    
+          <div className="flex flex-col gap-[10px]">
+            <h3 className="text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">Solutions</h3>
+            <p className="text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">
+              Flipbook Maker<br />Online Catalog<br />Magazine Maker<br />Brochure Maker<br />PDF Page Flip Effect
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-[10px]">
+            <h3 className="text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">&nbsp;</h3>
+            <p className="text-white/40 text-base font-semibold font-['Urbanist'] leading-[30px]">
+              Business Proposal<br />Ebook Maker<br />Multimedia Presentation<br />Company Newsletter Maker
+            </p>
+          </div>
         </div>
-        <div className="self-stretch opacity-40 text-center text-white/50 text-sm font-bold font-['Urbanist'] uppercase leading-[30px]">All-in-one Flipbook Maker</div>
-      </div> <div className="text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">English</div>
-      <div className="opacity-40 justify-start items-center gap-[15px] flex">
-        <div className="text-white text-base font-bold font-['Urbanist'] uppercase leading-[30px]">Follow Us:</div>
-        <div className="text-white text-base font-bold font-['Urbanist'] leading-[30px]">Facebook  |  Linkedin</div>
-      </div>
-    </div>
-  </div>
-  </div>
 
-  
+        <div className="flex justify-between items-center pb-[30px]">
+          <div className="flex items-center gap-2.5 opacity-40">
+            <div className="w-6 h-6 bg-[url('/local.svg')] bg-cover" />
+            <div className="flex items-center gap-[5px]">
+              <span className="text-white text-base font-bold font-['Urbanist'] uppercase">English</span>
+              <div className="w-6 h-6 bg-[url('/arrow2.svg')] bg-cover" />
+            </div>
+          </div>
+
+
+          <div className="text-center opacity-40 text-white/50 text-sm font-bold font-['Urbanist'] uppercase leading-[30px]">
+            <p>Convert your PDF into HTML5 Online flipbook with PAGE FLIP EFFECT.</p>
+            <p>All-in-one Flipbook Maker</p>
+          </div>
+
+          <div className="flex items-center gap-[15px] opacity-40">
+            <span className="text-white text-base font-bold font-['Urbanist'] uppercase">Follow Us:</span>
+            <span className="text-white text-base font-bold font-['Urbanist'] leading-[30px]">Facebook | Linkedin</span>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
